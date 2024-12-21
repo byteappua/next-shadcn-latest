19c356738098f5c6b51cca403577f18a7131767f
Aiden Dong <byteappua@gmail.com>
2024-12-21 15:19:09 +0800

    Enhance ExamplesLayout with SidebarInset and Breadcrumb Navigation
    - Introduced SidebarInset and SidebarTrigger components to improve layout structure.
    - Added Breadcrumb navigation for better user experience and navigation clarity.
    - Refactored the layout to encapsulate children within SidebarInset, maintaining sidebar functionality.
    - Cleaned up import statements for better organization and readability.

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,16 @@
 import { Metadata } from "next";
 import { AppSidebar } from "./components/app-sidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { cookies } from "next/headers";
+import { Separator } from "@/components/ui/separator";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 export const metadata: Metadata = {
   title: "Examples",
   description: "Check out some examples app built using the components.",
@@ -16,22 +25,28 @@ export default async function ExamplesLayout({ children }: ExamplesLayoutProps) 
   const sidebarStatus = cookieStore.get("sidebar:state");
   const defaultSidebarOpen = sidebarStatus ? sidebarStatus.value === "true" : true;
   return (
-    <SidebarProvider
-      defaultOpen={defaultSidebarOpen}
-      style={
-        {
-          "--sidebar-width": "8rem",
-          "--sidebar-width-mobile": "8rem",
-        } as React.CSSProperties
-      }
-    >
-      <section className="h-full w-full flex flex-row">
-        <AppSidebar />
-        <h1>helo</h1>
-        <div className="flex-1 overflow-hidden rounded-[0.5rem] border bg-background shadow flex flex-col">
-          {children}
-        </div>
-      </section>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b">
+          <div className="flex items-center gap-2 px-3">
+            <SidebarTrigger />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        {children}
+      </SidebarInset>
     </SidebarProvider>
   );
 }
