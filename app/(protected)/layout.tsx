2e4903af0ba051d417362b3f88bb77fff3b00b69
Aiden Dong <byteappua@gmail.com>
2024-12-21 15:13:34 +0800

    Implement sidebar functionality with cookie-based state management in ExamplesLayout
    - Added AppSidebar component and SidebarProvider to manage sidebar state.
    - Integrated cookie handling to determine the default open state of the sidebar.
    - Refactored layout structure to accommodate the new sidebar implementation.

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
-
+import { AppSidebar } from "./components/app-sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { cookies } from "next/headers";
 export const metadata: Metadata = {
   title: "Examples",
   description: "Check out some examples app built using the components.",
@@ -10,13 +12,26 @@ interface ExamplesLayoutProps {
 }
 
 export default async function ExamplesLayout({ children }: ExamplesLayoutProps) {
+  const cookieStore = await cookies();
+  const sidebarStatus = cookieStore.get("sidebar:state");
+  const defaultSidebarOpen = sidebarStatus ? sidebarStatus.value === "true" : true;
   return (
-    <div className="flex-1">
-      <section className="h-full flex flex-col">
+    <SidebarProvider
+      defaultOpen={defaultSidebarOpen}
+      style={
+        {
+          "--sidebar-width": "8rem",
+          "--sidebar-width-mobile": "8rem",
+        } as React.CSSProperties
+      }
+    >
+      <section className="h-full w-full flex flex-row">
+        <AppSidebar />
+        <h1>helo</h1>
         <div className="flex-1 overflow-hidden rounded-[0.5rem] border bg-background shadow flex flex-col">
           {children}
         </div>
       </section>
-    </div>
+    </SidebarProvider>
   );
 }
