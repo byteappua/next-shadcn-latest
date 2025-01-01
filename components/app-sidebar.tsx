a15c9c2a9085c8db3b8239dd5b9f7a3d00604a82
Aiden Dong <byteappua@gmail.com>
2025-01-01 14:13:00 +0800

    1

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -65,7 +65,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               <SidebarMenuItem key={subItem.name}>
                 <SidebarMenuSubButton asChild>
                   <Link href={subItem.url}>
-                    {subItem.icon && <subItem.icon />}
+                    {/* {subItem.icon && <subItem.icon />} */}
                     {subItem.name}
                   </Link>
                 </SidebarMenuSubButton>
